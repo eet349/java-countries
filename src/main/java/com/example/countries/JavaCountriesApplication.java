1ba98e6502dbcd264aae0bfbd61b5e23b1b2fe07
eet349 <51262590+eet349@users.noreply.github.com>
2021-04-08 21:50:40 -0500

    Setting up project.

---
+++ src/main/java/com/example/countries/JavaCountriesApplication.java
@@ -0,0 +1,13 @@
+package com.example.countries;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaCountriesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaCountriesApplication.class, args);
+    }
+
+}
